9e7c3380f51d5a8852bd3e06d2c0780840b0ffa9
Sasheem <sts12@me.com>
2019-01-30 02:46:57 -0500

    made action and reducers for selected songs from a song list

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [{
+        { title: 'MIDDLE KID', duration: '4:05' },
+        { title: 'Wings', duration: '3:45' },
+        { title: 'Thriller', duration: '7:10' },
+        { title: 'In the morning', duration: '3:59' }
+    }];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+};
